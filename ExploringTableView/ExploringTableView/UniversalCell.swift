c5b199455a228a4d18262f26db9a506b25c2304d
alexei-zhuravlev <70209975+alexei-zhuravlev@users.noreply.github.com>
2022-03-17 11:32:25 +0300

    Create UniversalCell.swift

---
+++ ExploringTableView/ExploringTableView/UniversalCell.swift
@@ -0,0 +1,24 @@
+//
+//  UniversalCell.swift
+//  ExploringTableView
+//
+//  Created by Алексей Журавлев on 16.03.2022.
+//
+
+import UIKit
+
+class UniversalCell: UITableViewCell {
+
+    @IBOutlet weak var userName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
