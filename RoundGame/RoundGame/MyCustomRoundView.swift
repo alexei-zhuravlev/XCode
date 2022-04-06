bbcb4775504ddda545de65cf279c2ac6be325b0c
alexei-zhuravlev <70209975+alexei-zhuravlev@users.noreply.github.com>
2022-04-06 14:34:08 +0300

    Create MyCustomRoundView.swift

---
+++ RoundGame/RoundGame/MyCustomRoundView.swift
@@ -0,0 +1,57 @@
+//
+//  MyCustomRoundView.swift
+//  RoundGame
+//
+//  Created by Алексей Журавлев on 31.03.2022.
+//
+
+import UIKit
+
+@IBDesignable class MyCustomRoundView: UIView {
+
+    var workingView: UIView!
+    var xibName = "MyCustomRoundView"
+    
+    let colorDarkBlue = UIColor(red: 51.0/255.0, green: 102.0/255.0, blue: 153.0/255.0, alpha: 1.0)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setCustomView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setCustomView()
+    }
+    
+    func getFromXib() -> UIView{
+        
+        let bundle = Bundle(for: type(of: self))
+        let xib = UINib(nibName: xibName, bundle: bundle)
+        let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return view
+    }
+    
+    func setCustomView(){
+        workingView = getFromXib()
+        workingView.frame = bounds
+        workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        workingView.layer.cornerRadius = frame.size.width/2
+        
+        addSubview(workingView)
+    }
+    
+    func changingSize(){
+        workingView.frame.size.height += 10
+        workingView.frame.size.width += 10
+        workingView.layer.cornerRadius = workingView.frame.size.width/2
+    }
+    
+    func changingColor(){
+        workingView.backgroundColor = colorDarkBlue
+    }
+    
+
+}
