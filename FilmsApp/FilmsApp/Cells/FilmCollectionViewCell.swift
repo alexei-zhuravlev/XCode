fd49fb39979022f0d3e217ac17ff7ce0d60322d1
alexei-zhuravlev <70209975+alexei-zhuravlev@users.noreply.github.com>
2022-10-24 19:57:18 +0300

    Fisrt Step Final Project

---
+++ FilmsApp/FilmsApp/Cells/FilmCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FilmCollectionViewCell.swift
+//  FilmsApp
+//
+//  Created by Алексей Журавлев on 24.10.2022.
+//
+
+import UIKit
+
+class FilmCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterPreviewImageView: UIImageView!
+    @IBOutlet weak var filmTitleLabel: UILabel!
+    @IBOutlet weak var releaseYearLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+}
